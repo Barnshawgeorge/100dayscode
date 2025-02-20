2076e770c827b8b0ceda4655ed1cee27c7494d19
Barnshawgeorge <barnshawgeorge@gmail.com>
2025-02-19 19:38:59 -0600

    Day 1

---
+++ d1_NameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+user_input_name = input("What's the name of thecity you grew up in?\n")
+user_input_petname = input("What is your pets name \n")
+print(f"Your band name should be {user_input_name} {user_input_petname}")
