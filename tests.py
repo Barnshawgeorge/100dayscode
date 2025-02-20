3b4d3b78c274ba2dcb09e601ada3500cf025e26d
Barnshawgeorge <barnshawgeorge@gmail.com>
2025-02-20 11:08:32 -0600

    day5

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
-a = ["a", "b", "c", "d"]
-
-num_list = len(a)
-
-print(a[num_list -1])
+for i in range(1, 101):
+    if(i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    elif(i % 3 == 0):
+        print("Fizz")
+    elif(i % 5 == 0):
+        print("Buzz")
+    else:
+        print(i)
